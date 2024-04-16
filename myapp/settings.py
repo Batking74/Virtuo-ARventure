684b62854e6c12d99b8fc12abf6a043574cb42bf
Nazir Knuckles <nazirknuckles@gmail.com>
2024-04-16 17:25:05 -0400

    added some more settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ko8t@*027c7t-@bcv!zradrz*m@=ws@_x37!yoqz46&0u-$j4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['naz-first-fullstack-python-app.onrender.com', 'localhost']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,9 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = BASE_DIR / 'DeployableStaticAssets'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
